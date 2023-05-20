ff0792266474b932570c9996ce9220fae8a89db9
Laslo Kozlov <iamlaslo@icloud.com>
2023-05-20 13:41:48 +0300

    Initial Commit

---
+++ PromovaTest/PromovaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromovaTestApp.swift
+//  PromovaTest
+//
+//  Created by Laslo on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PromovaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
